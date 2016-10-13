f2e9653d609511e91fe81e6540aadffa02572cea
Luke Sedmak <luke@lukesedmak.com>
2016-10-12 20:12:16 -0400

    removed Jeremy and Robb

--- artists.php
+++ artists.php
@@ -8,33 +8,21 @@
 				</a>
 			</article>
 			<article class="even">
-				<a href="http://instagram.com/everynamestaken" target="_blank">
-					<img src="img/jeremy.jpg" alt="Tattoo Artist Jeremy McGrady - Cloak and Dagger Tattoo, Dayton OH" />
-					<h1>Jeremy McGrady</h1>
-				</a>
-			</article>
-			<article class="odd">
 				<a href="http://instagram.com/whiskeyadam" target="_blank">
 					<img src="img/adam.jpg" alt="Tattoo Artist Adam Brockman - Cloak and Dagger Tattoo, Dayton OH" />
 					<h1>Adam Brockman</h1>
 				</a>
 			</article>
-			<article class="even">
+			<article class="odd">
 				<a href="http://instagram.com/bangorangdayton" target="_blank">
 					<img src="img/jess.jpg" alt="Tattoo Artist Jess Oram - Cloak and Dagger Tattoo, Dayton OH" />
 					<h1>Jess Oram</h1>
 				</a>
 			</article>
-			<article class="odd">
+			<article class="even">
 				<a href="http://instagram.com/profes_plum" target="_blank">
 					<img src="img/steve.jpg" alt="Tattoo Artist Steve Ratcliffe - Cloak and Dagger Tattoo, Dayton OH" />
 					<h1>Steve Ratcliffe</h1>
-				</a>
-			</article>
-			<article class="even">
-				<a href="http://instagram.com/robwilliams_cloakanddagger" target="_blank">
-					<img src="img/rob.jpg" alt="Tattoo Artist Rob Williams - Cloak and Dagger Tattoo, Dayton OH" />
-					<h1>Rob Williams</h1>
 				</a>
 			</article>
 		</section>
